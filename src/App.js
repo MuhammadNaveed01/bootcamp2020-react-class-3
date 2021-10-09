ed642a062e8ab6a88a810d42f98ab9be702117bb
MuhammadNaveed01 <72243722+MuhammadNaveed01@users.noreply.github.com>
2021-10-09 12:27:32 +0500

    greeting message
    greeting message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './component/Greeting';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting name="Superman" heroName="Clark"></Greeting>
+      <Greeting name="Batmen" heroName="Bruce"></Greeting>
+      <Greeting name="Wonderwomen" heroName="Diana"></Greeting>
     </div>
   );
 }
---
+++ src/component/Greeting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greeting({name, heroName}) {
+    return (
+        <div>
+            <h2>Greeting {name} a.k.a {heroName}</h2>
+        </div>
+    )
+}
+
+export default Greeting
